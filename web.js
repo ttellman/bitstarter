fa73cc65fb44a937a09c0e7baa9ad72df272bdf8
ttellman <ttellmandesign@gmail.com>
2013-07-09 07:14:33 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
+var fs = fs.readFileSync('/index.html');
+var buffer = new buffer (fs);
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
